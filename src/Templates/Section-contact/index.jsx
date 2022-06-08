ea562cc51bb7b33eb7fb77f2920434be248e7633
Eros Chagas <eroschagas@hotmail.com>
2022-06-08 04:04:19 -0300

    contact concept

--- src/Templates/Section-contact/index.jsx
+++ src/Templates/Section-contact/index.jsx
@@ -3,6 +3,16 @@ export const Contact = () => {
     <section id="contact">
       <div className="general-spacing"></div>
       <h1 className="contact-title">Contato</h1>
+      <div className="contact-info">
+        <h1>Title</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing
+          elit. Neque voluptatum recusandae sapiente, quod
+          illo dolores debitis rem libero accusamus, ipsa
+          placeat laborum, quibusdam excepturi quos. Aliquam
+          officia at vero incidunt.
+        </p>
+      </div>
     </section>
   );
 };
